f79182b66df6aa9e7825a27014d25cda2ce9e221
monnie26 <108433532+monnie26@users.noreply.github.com>
2023-10-17 21:45:45 +0530

    Add files via upload

---
+++ WebWizardry/components/footer.js
@@ -0,0 +1,356 @@
+function footer (){
+    return `
+    <!-- top category here  -->
+
+<div class="top_div_fns">
+    <div class="headings_fns" >
+        <h2>TOP CATEGORY</h2>
+    </div>
+    <div class="top_cate_Content_fns">
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/explained/"> Explained News</a>   
+
+            </li>
+
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/subscribe/"> Political Pulse</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/opinion/"> Latest Opinion News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/cities/mumbai/"> Mumbai News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/cities/delhi/"> Delhi News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/cities/pune/"> Pune News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/cities/banglore/"> Banglore News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/cities/mumbai/"> Bollywood News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/explained/"> Health News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/explained/"> India News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/sports/"> Sports News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/lifestyle/"> Lifestyle News</a>   
+            </li>
+        </div>
+        
+    </div>
+</div>
+
+<!-- // trending here -->
+
+<div class="top_div_fns">
+    <div class="headings_fns" >
+        <h2>TRENDING NEWS</h2>
+    </div>
+    <div class="top_cate_Content_fns">
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/horoscope/"> Horoscope</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/india/">India News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://eureka.indianexpress.com/video/art-craft/">Art & Craft Classes For Kids</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://eureka.indianexpress.com/video/fitness/"> Fitness Classes For Kids</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://eureka.indianexpress.com/video/dance/">Dance Classes For Kids</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/entertainment/"> Entertainment News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/upsc-current-affairs/">UPSC News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/section/sports/ipl">IPL 2022 News</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/subscribe/all-access/">Buy All-Access Pass</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/subscribe/">Subscribe To The Indian Express</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/subscribe/premium/">Buy Digital Premium</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/article/entertainment/bollywood/amitabh-bachchan-says-covid-won-as-he-provides-health-update-8110333/">Amitabh Bachchan Covid Positive</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/article/entertainment/web-series/pankaj-tripathi-wants-to-break-out-of-comfort-zone-says-acting-for-him-is-a-mode-of-feeling-happiness-and-earning-a-livelihood-8097748/">Pankaj Tripathi Wants To 'Break <span style="color: black;">.........</span> Out    Of His Comfort Zone'</a>   
+            </li>
+        </div>
+        
+        <div>
+            <li>
+                <a href="https://indianexpress.com/article/entertainment/movie-review/orphan-first-kill-movie-review-isabelle-furhman-horror-film-8108986/">Orphan First Kill Movie Review</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/photos/entertainment-gallery/vikram-vedha-everything-you-need-to-know-about-saif-ali-khan-and-hrithik-roshan-film-8108928/">Vikram Vedha: Everything You <span style="color: black;">.........</span> Need To Know</a>   
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="https://indianexpress.com/article/sports/badminton/bwf-world-championships-day-4-live-8109909/">BWF World Championships Day 4 <span style="color: black;">.........</span> Live</a>   
+            </li>
+        </div>
+    </div>
+</div>
+
+<!-- Latest Stories here  -->
+
+
+<!-- // footer below part here  -->
+
+<div id="footer_blw_ns">
+    <div id="footer_blw_ns1" > 
+        <div id="footer_blw_sub_ns1" >
+            <div class="soc_heads_ns">
+            FOLLOW US
+            </div>
+            <div class="soc_img_div_ns">
+                <div id="media_icons_ns">
+                   <a href="https://www.facebook.com/indianexpress">
+                    <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/icons/icon-facebook.svg" alt="">
+                   </a> 
+
+                   <a href="https://twitter.com/indianexpress">
+                    <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/icons/icon-twitter.svg" alt="">
+                   </a>
+
+                   <a href="https://www.linkedin.com/company/indian-express/">
+                    <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/icons/icon-linkedin.svg" alt="">
+                   </a>
+
+                   <a href="https://www.instagram.com/indianexpress/">
+                    <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/icons/icon-instagram.svg" alt="">
+                    </a>
+
+                </div>
+            </div>
+
+            <!-- // second media -->
+            <div class="soc_heads_ns">
+                DOWNLOAD APPS
+                </div>
+                <div class="soc_img_div_ns">
+                    <div id="media_icons_ns">
+                        <a href="https://play.google.com/store/apps/details?id=com.indianexpress.android&hl=en">
+                        <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/icons/icon-android.svg" alt="">
+                        </a>
+
+                        <a href="https://itunes.apple.com/us/app/the-indian-express/id506351833?mt=8">
+                        <img src="https://indianexpress.com/wp-content/themes/indianexpress/images/icons/icon-apple.svg" alt="">
+                        </a>
+                        
+                    </div>
+                </div>
+        </div>
+        <div id="footer_blw_sub1_ns1" >
+
+           
+
+            <div id="main_newsguard_ns">
+                
+                <div id="newGuard_con_ns" >
+                    <p>The Indian Express website has been rated GREEN for its credibility and trustworthiness by Newsguard, a global service that rates news sources for their journalistic standards.</p>
+                </div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+    <div id="footer_blw_ns2" >
+        <div class="footer_blw_head_ns2">
+            EXPRESS GROUP
+        </div>    
+        <div id="footer_blw_Con_ns2">
+            <div>
+                <a href="https://indianexpress.com/">The Indian Express</a>
+            </div>
+
+            <div>
+                <a href="https://tamil.indianexpress.com/">ieTamil.com</a>
+
+            </div>
+            <div>
+                <a href="https://www.financialexpress.com/">The Financial Express</a>
+            </div>
+            <div>
+                <a href="https://bengali.indianexpress.com/">ieBangla.com</a>
+            </div>
+            <div>
+                <a href="https://www.loksatta.com/">Loksatta</a>
+            </div>
+            <div>
+                <a href="https://malayalam.indianexpress.com/">ieMalayalam.com</a>
+            </div>
+            <div>
+                <a href="https://www.jansatta.com/">Jansatta</a>
+            </div>  
+            <div>
+                <a href="https://www.inuth.com/">inUth</a>
+            </div>  
+            <div>
+                <a href="https://expressgroup.indianexpress.com/">The ExpressGroup</a>
+            </div>  
+            <div>
+                <a href="https://www.myinsuranceclub.com/">MyInsuranceClub</a>
+            </div>  
+            <div>
+                <a href="https://indianexpress.com/newsletters/">Newsletters</a>
+            </div>  
+        
+            <div>
+                <a href="https://www.lighthousejournalism.com/">Light House Journalism</a>
+            </div>  
+        
+            <div>
+                <a href="https://www.myinsuranceclub.com/life-insurance/term-insurance/">
+                    Compare Term Insurance</a>
+            </div>  
+        
+        </div>    
+    </div>
+    <div id="footer_blw_ns3" > 
+        <div class="footer_blw_head_ns2">
+            QUICK LINKS
+        </div> 
+        <div id="footer_blw_Con_quick_ns2">
+            <div>
+                <a href="https://indianexpress.com/terms-and-conditions/">
+                    T&C</a>
+            </div> 
+
+            <div>
+                <a href="https://indianexpress.com/privacy-policy/">
+                    Privacy Policy</a>
+            </div> 
+
+            <div>
+                <a href="https://expressgroup.indianexpress.com/advertise.html">
+                    Advertise with Us</a>
+            </div> 
+
+            <div>
+                <a href="https://indianexpress.com/brand-solutions/">
+                    Brand Solutions</a>
+            </div> 
+
+            <div>
+                <a href="https://indianexpress.com/contact-us/">
+                    Contact Us</a>
+            </div> 
+
+            <div>
+                <a href="https://indianexpress.com/article/what-is/what-is-the-protection-of-children-from-sexual-offences-act-2012/">
+                    Statutory provisions on reporting (sexual offenses)</a>
+            </div> 
+
+            <div>
+                <a href="https://indianexpress.com/article/india/code-of-ethics-for-digital-news-websites-6758543/">
+                    This website follows the DNPA’s code of conduct</a>
+            </div> 
+
+        </div>  
+        
+    </div>
+</div>`
+
+
+}
+
+export default footer
---
+++ WebWizardry/components/nav.js
@@ -0,0 +1,7 @@
+function navbar(){
+    return `  <a href="index.html"> <i class="fa fa-long-arrow-left" style="font-size:8px"></i> Back to Indian Express</a>
+    `
+    
+}
+
+export default navbar
